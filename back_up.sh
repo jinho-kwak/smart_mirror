ea8b4c766f52289aad79cb541c1470ee2d467b2c
jinho-kwak <79895275+jinho-kwak@users.noreply.github.com>
2022-03-03 15:18:18 +0900

    Update back_up.sh

--- back_up.sh
+++ back_up.sh
@@ -3,12 +3,12 @@ now_dir_path=`pwd -P`
 CUR_DATE='backup_'`date +%Y%m%d`
 mkdir -p /home/ubuntu/$CUR_DATE
 
-cp -r /home/ubuntu/smart_retail /home/ubuntu/$CUR_DATE
+cp -r /home/ubuntu/smart_mirror /home/ubuntu/$CUR_DATE
 
-cd /home/ubuntu/smart_retail/
-rm -rf `ls /home/ubuntu/smart_retail/ | grep -v logs | grep -v keys`
+cd /home/ubuntu/smart_mirror/
+rm -rf `ls /home/ubuntu/smart_mirror/ | grep -v logs | grep -v keys`
 rm -rf .git
 
 cd $now_dir_path
-cp -i -rf smart_retail/* /home/ubuntu/smart_retail/
-cp -i -rf smart_retail/.git /home/ubuntu/smart_retail/
+cp -i -rf smart_mirror/* /home/ubuntu/smart_mirror/
+cp -i -rf smart_mirror/.git /home/ubuntu/smart_mirror/
